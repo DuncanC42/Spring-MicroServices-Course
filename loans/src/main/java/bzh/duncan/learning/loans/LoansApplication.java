941ee445f6af0abec6042c6529800d969fd5540a
DuncanC42 <cadoret.du@gmail.com>
2024-07-05 13:27:15 +0200

    profiles manager (need to be implemented for Loans and Cards

--- loans/src/main/java/bzh/duncan/learning/loans/LoansApplication.java
+++ loans/src/main/java/bzh/duncan/learning/loans/LoansApplication.java
@@ -1,11 +1,41 @@
 package bzh.duncan.learning.loans;
 
+import bzh.duncan.learning.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Duncan Cadoret",
+                        email = "duncancadoret@laposte.net",
+                        url = "www.google.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "www.google.fr"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "http://localhost:8080/swagger-ui/index.html"
+        )
+)
 public class LoansApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(LoansApplication.class, args);
